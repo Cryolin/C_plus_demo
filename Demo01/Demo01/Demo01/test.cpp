7b508048c0fa1b5e913f59d4ed18c0936a2eec72
Cryolin <siyu@mail.ustc.edu.cn>
2021-06-13 16:18:26 +0800

    更新项目结构

--- Demo01/Demo01/Demo01/test.cpp
+++
@@ -1,8 +0,0 @@
-// 自定义头问价，通过双引号导入
-#include "test.h"
-
-int test(int a, int b)
-{
-	cout << "test" << endl;
-	return 0;
-}
--- Demo01/Demo01/Demo01/test.h
+++
@@ -1,5 +0,0 @@
-#include <iostream>;
-
-using namespace std;
-
-int test(int a, int b);
