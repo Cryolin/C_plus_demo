291b704bf078ea518107e2ae20820673b0a85352
Cryolin <siyu@mail.ustc.edu.cn>
2021-06-13 15:59:02 +0800

    完成C++基础编程部分

--- Demo01/Demo01/Demo01/11. 函数分文件编写.cpp
+++ Demo01/Demo01/Demo01/11. 函数分文件编写.cpp
@@ -7,14 +7,14 @@
 // test.h中，已经使用了std的命名空间，无需重复声明
 //using namespace std;
 
-int main() {
-	int a = 1;
-	int b = 2;
-	int c = test(a, b);
-	cout << "main" << endl;
-	system("pause");
-	return 0;
-}
+//int main() {
+//	int a = 1;
+//	int b = 2;
+//	int c = test(a, b);
+//	cout << "main" << endl;
+//	system("pause");
+//	return 0;
+//}
 
 // 所有导入test.h头文件的cpp文件，只能有一个test()函数的定义
 // 下方如果重复定义会报错
---
+++ Demo01/Demo01/Demo01/16. 结构体的定义和使用.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+//// 结构体定义
+//struct Student
+//{
+//	string name;
+//	int age;
+//	int score;
+//}s3; // 创建结构体变量方法3
+//
+//int main() {
+//	// 创建结构体变量的三种方法
+//	
+//	// 1. struct 结构体名 变量名
+//	struct Student s1;
+//	// Student s1; 创建时，struct是可以省略的
+//	s1.name = "张三";	// 结构体变量通过"."访问成员
+//	s1.age = 18;
+//	s1.score = 100;
+//
+//
+//	// 2. struct 结构体名 变量名 = {成员1值，成员2值...}
+//	Student s2 = { "李四", 19, 80 };
+//
+//	// 3. 在定义结构体时顺便创建变量
+//	s3.name = "王五";
+//	s3.age = 20;
+//	s3.score = 60;
+//
+//	system("pause");
+//	return 0;
+//}
