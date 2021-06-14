e55a57f5d867bc6a2d46e41738b24d8de1786958
Cryolin <siyu@mail.ustc.edu.cn>
2021-06-14 21:50:35 +0800

    内存分配相关

---
+++ 2. C++核心编程/1. 静态内存和动态内存.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+//int g_a = 10;
+//static int s_g_a = 10;
+//const int c_g_a = 10;
+//
+//int main()
+//{
+//	int l_a = 10;
+//	static int s_l_a = 10;
+//	const int c_l_a = 10;
+//
+//	/*
+//	普通局部变量的地址0078FE5C
+//	普通全局变量的地址00DBC000
+//	静态局部变量的地址00DBC008
+//	静态全局变量的地址00DBC004
+//	const全局常量的地址00DB9B44
+//	const局部常量的地址0078FE50
+//	*/
+//	cout << "普通局部变量的地址" << &l_a << endl;
+//	cout << "普通全局变量的地址" << &g_a << endl;
+//	cout << "静态局部变量的地址" << &s_l_a << endl;
+//	cout << "静态全局变量的地址" << &s_g_a << endl;
+//	cout << "const全局常量的地址" << &c_g_a << endl;
+//	cout << "const局部常量的地址" << &c_l_a << endl;
+//
+//	// 静态分配的有：全局变量和静态变量
+//
+//	system("pause");
+//	return 0;
+//}
---
+++ 2. C++核心编程/2. 栈.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+//int* getInt()
+//{
+//	int a = 10;
+//	return &a;
+//}
+//
+//int main()
+//{
+//	int* a = getInt();
+//
+//	// 10			第一行打印10
+//	// 2084673928	第二行打印时，内存已被回收
+//	cout << *a << endl;
+//	cout << *a << endl;
+//
+//	system("pause");
+//	return 0;
+//}
