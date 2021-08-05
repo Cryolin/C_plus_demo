50b25e52235f96457127f5218d554101621a0335
Cryolin <siyu@mail.ustc.edu.cn>
2021-08-06 00:48:14 +0800

    malloc&free

--- 3. C++提高编程/1. 函数模板基本语法.cpp
+++ 3. C++提高编程/1. 函数模板基本语法.cpp
@@ -1,51 +1,51 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//// 函数模板01
-//template<typename T>
-//void func()
-//{
-//	cout << sizeof(T) << endl;
-//}
-//
-//// 函数模板02
-//template<class T>
-//void mySwap(T& a, T& b)
-//{
-//	T temp = a;
-//	a = b;
-//	b = temp;
-//}
-//
-//void test01()
-//{
-//	int a = 10;
-//	int b = 20;
-//	char c = 'c';
-//	// 函数模板的使用方式一：自动类型推导
-//	mySwap(a, b);
-//	// 函数模板的使用方式二：显示指定类型
-//	mySwap<int>(a, b);
-//
-//	// 函数模板必须可以推导出T的数据类型，如下代码报错
-//	//mySwap(a, c);
-//	// 同样报错
-//	//mySwap<char>(a, c);
-//}
-//
-//void test02()
-//{
-//	func<char>();
-//
-//	// 函数模板在不确定时，无法执行，如下代码报错
-//	//func();
-//}
-//
-//int main() {
-//	test01();
-//	test02();
-//
-//	system("pause");
-//	return 0;
-//}
+#include <iostream>
+
+using namespace std;
+
+// 函数模板01
+template<typename T>
+void func()
+{
+	cout << sizeof(T) << endl;
+}
+
+// 函数模板02
+template<class T>
+void mySwap(T& a, T& b)
+{
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+void test01()
+{
+	int a = 10;
+	int b = 20;
+	char c = 'c';
+	// 函数模板的使用方式一：自动类型推导
+	mySwap(a, b);
+	// 函数模板的使用方式二：显示指定类型
+	mySwap<int>(a, b);
+
+	// 函数模板必须可以推导出T的数据类型，如下代码报错
+	//mySwap(a, c);
+	// 同样报错
+	//mySwap<char>(a, c);
+}
+
+void test02()
+{
+	func<char>();
+
+	// 函数模板在不确定时，无法执行，如下代码报错
+	//func();
+}
+
+int main() {
+	test01();
+	test02();
+
+	system("pause");
+	return 0;
+}
---
+++ 4. C++实用编程/12. malloc和free.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+void test01()
+{
+	// 直接在栈区开辟大数组，报Stack Overflow，如下代码报错
+	//int arr[10 * 1024 * 1024];
+
+	// 通过malloc函数堆空间开辟内存，返回内存的首地址
+	// 返回值类型是void*，可以强转为任意指针
+	int* arr = (int*) malloc(10 * 1024 * 1024 * sizeof(int));
+
+	// 开辟完内存后，可以直接作为int数组写入
+	arr[0] = 1;
+	arr[1] = 2;
+
+	// 使用完后记得调用free释放内存空间
+	free(arr);
+
+	// 请注意，如下方式定义的int指针是不能作为数组使用的，未进行数组初始化
+	//int* arr2;
+	//arr2[1] = 10;
+
+	// 这样是可以的
+	int* arr3 = new int[10];
+	arr3[1] = 1;
+	delete[] arr3;
+}
+
+// 
+void test02()
+{
+	int a = 1;
+
+	// 变量不能直接用作数组初始化，如下代码报错
+	//int arr[a];
+
+	// 如果想通过变量初始化数组大小，可以使用new关键字在堆区创建
+	int* arr = new int[a];
+	arr[0] = 1;
+	delete[] arr;
+
+	// 当然，malloc也是可以的
+	int* arr2 = (int*)malloc(sizeof(int)*a);
+	arr2[0] = 1;
+	// 使用完后记得调用free释放内存空间
+	free(arr2);
+}
+
+int main(void)
+{
+	test01();
+
+	test02();
+
+	system("pause");
+	return 0;
+}
