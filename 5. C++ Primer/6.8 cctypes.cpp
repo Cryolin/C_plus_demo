a3f29d721d638260f7f580ca9ac6090bd706f0d5
Cryolin <siyu@mail.ustc.edu.cn>
2022-09-11 22:00:59 +0800

    运算符

---
+++ 5. C++ Primer/6.8 cctypes.cpp
@@ -0,0 +1,38 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	cout << "Enter text for analysis, and type @"
+//		" to terminate input.\n";
+//	char ch;
+//	int whitespace = 0;
+//	int digits = 0;
+//	int chars = 0;
+//	int punct = 0;
+//	int others = 0;
+//
+//	while (ch != '@')
+//	{
+//		if (isalpha(ch))
+//			chars++;
+//		else if (isspace(ch))
+//			whitespace++;
+//		else if (isdigit(ch))
+//			digits++;
+//		else if (ispunct(ch))
+//			punct++;
+//		else
+//			others++;
+//		cin.get(ch);
+//	}
+//
+//	cout << chars << " letters, "
+//		<< whitespace << " whitespace, "
+//		<< digits << " digits, "
+//		<< punct << " punctuations, "
+//		<< others << " others.\n";
+//
+//	system("pause");
+//	return 0;
+//}
