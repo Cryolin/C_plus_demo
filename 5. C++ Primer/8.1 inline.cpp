dd73c6104a505679b510e4eda4882f17ab1ae4a9
Cryolin <siyu@mail.ustc.edu.cn>
2022-10-15 22:11:38 +0800

    引用

--- 5. C++ Primer/8.1 inline.cpp
+++ 5. C++ Primer/8.1 inline.cpp
@@ -1,20 +1,20 @@
-#include <iostream>
-using namespace std;
-
-inline double square(double x) { return x * x; };
-
-int main()
-{
-	double a, b;
-	double c = 13.0;
-
-	a = square(5.0);
-	b = square(4.5 + 7.5);
-	cout << "a = " << a << ", b = " << b << "\n";
-	cout << "c = " << c;
-	cout << ", c squared = " << square(c++) << "\n";
-	cout << "Now c = " << c << "\n";
-
-	system("pause");
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//inline double square(double x) { return x * x; };
+//
+//int main()
+//{
+//	double a, b;
+//	double c = 13.0;
+//
+//	a = square(5.0);
+//	b = square(4.5 + 7.5);
+//	cout << "a = " << a << ", b = " << b << "\n";
+//	cout << "c = " << c;
+//	cout << ", c squared = " << square(c++) << "\n";
+//	cout << "Now c = " << c << "\n";
+//
+//	system("pause");
+//	return 0;
+//}
---
+++ 5. C++ Primer/8.4 swaps.cpp
@@ -0,0 +1,50 @@
+//#include <iostream>
+//using namespace std;
+//void swapr(int& a, int& b);
+//void swapp(int* p, int* q);
+//void swapv(int a, int b);
+//
+//int main()
+//{
+//	int wallet1 = 300;
+//	int wallet2 = 350;
+//	cout << "wallet1 = $" << wallet1;
+//	cout << " wallet2 = $" << wallet2 << endl;
+//	
+//	cout << "Using references to swap contents:\n";
+//	swapr(wallet1, wallet2);
+//	cout << "wallet1 = $" << wallet1;
+//	cout << " wallet2 = $" << wallet2 << endl;
+//
+//	cout << "Using pointers to swap contents:\n";
+//	swapp(&wallet1, &wallet2);
+//	cout << "wallet1 = $" << wallet1;
+//	cout << " wallet2 = $" << wallet2 << endl;
+//
+//	cout << "Trying to use passing by value:\n";
+//	swapv(wallet1, wallet2);
+//	cout << "wallet1 = $" << wallet1;
+//	cout << " wallet2 = $" << wallet2 << endl;
+//
+//	system("pause");
+//	return 0;
+//}
+//
+//void swapr(int& a, int& b)
+//{
+//	int temp = a;
+//	a = b;
+//	b = temp;
+//}
+//void swapp(int* p, int* q)
+//{
+//	int temp = *p;
+//	*p = *q;
+//	*q = temp;
+//}
+//void swapv(int a, int b)
+//{
+//	int temp = a;
+//	a = b;
+//	b = temp;
+//}
