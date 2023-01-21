b42c67b86db3c94a814f14740f8b91c8ea69f347
Cryolin <siyu@mail.ustc.edu.cn>
2023-01-21 22:18:43 +0800

    类和动态内存分配

--- 5. C++ Primer/11.7 complex.h
+++ 5. C++ Primer/11.7 complex.h
@@ -1,27 +1,27 @@
-#ifndef COMPLEX_H_
-#define COMPLEX_H_
-
-#include <iostream>
-
-using namespace std;
-
-class Complex
-{
-private:
-	double real;
-	double imaginary;
-public:
-	Complex();
-	Complex(double r, double i);
-	~Complex();
-	Complex operator~() const;
-	Complex operator+(const Complex& complex) const;
-	Complex operator-(const Complex& complex) const;
-	Complex operator*(const Complex& complex) const;
-	Complex operator*(double n) const;
-	friend Complex operator*(double n, const Complex& complex);
-	friend ostream& operator<<(ostream& os, const Complex& complex);
-	friend istream& operator>>(istream& is, Complex& complex);
-};
-
-#endif
+//#ifndef COMPLEX_H_
+//#define COMPLEX_H_
+//
+//#include <iostream>
+//
+//using namespace std;
+//
+//class Complex
+//{
+//private:
+//	double real;
+//	double imaginary;
+//public:
+//	Complex();
+//	Complex(double r, double i);
+//	~Complex();
+//	Complex operator~() const;
+//	Complex operator+(const Complex& complex) const;
+//	Complex operator-(const Complex& complex) const;
+//	Complex operator*(const Complex& complex) const;
+//	Complex operator*(double n) const;
+//	friend Complex operator*(double n, const Complex& complex);
+//	friend ostream& operator<<(ostream& os, const Complex& complex);
+//	friend istream& operator>>(istream& is, Complex& complex);
+//};
+//
+//#endif
