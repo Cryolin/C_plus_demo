63c621d94abc462eae6cb74e7d6adb4f81bca5d9
Cryolin <siyu@mail.ustc.edu.cn>
2023-01-24 21:30:47 +0800

    类继承

--- 5. C++ Primer/12.4 stack.h
+++ 5. C++ Primer/12.4 stack.h
@@ -1,25 +1,25 @@
-#ifndef _STACK_H_
-#define _STACK_H_
-
-typedef unsigned long Item;
-
-class Stack
-{
-private:
-    enum { MAX = 10 };    // constant specific to class
-    Item* pitems;      // holds stack items
-    int size;
-    int top;          // index for top stack item
-public:
-    Stack(int n = MAX);
-    Stack(const Stack& st);
-    ~Stack();
-    bool isempty() const;
-    bool isfull() const;
-    // push() returns false if stack already is full, true otherwise
-    bool push(const Item& item);   // add item to stack
-    // pop() returns false if stack already is empty, true otherwise
-    bool pop(Item& item);          // pop top into item
-    Stack& operator=(const Stack& st);
-};
-#endif // _STACK_H_
+//#ifndef _STACK_H_
+//#define _STACK_H_
+//
+//typedef unsigned long Item;
+//
+//class Stack
+//{
+//private:
+//    enum { MAX = 10 };    // constant specific to class
+//    Item* pitems;      // holds stack items
+//    int size;
+//    int top;          // index for top stack item
+//public:
+//    Stack(int n = MAX);
+//    Stack(const Stack& st);
+//    ~Stack();
+//    bool isempty() const;
+//    bool isfull() const;
+//    // push() returns false if stack already is full, true otherwise
+//    bool push(const Item& item);   // add item to stack
+//    // pop() returns false if stack already is empty, true otherwise
+//    bool pop(Item& item);          // pop top into item
+//    Stack& operator=(const Stack& st);
+//};
+//#endif // _STACK_H_
--- 5. C++ Primer/12.4 usestack.cpp
+++ 5. C++ Primer/12.4 usestack.cpp
@@ -1,49 +1,49 @@
-#include <iostream>
-#include "12.4 stack.h"
-using namespace std;
-
-int main(void)
-{
-	Stack st1;
-	Stack st2;
-	Item p1[10] = { 12,20,3,0,2,2,3,5,8,9 };
-	for (int i = 0; i < 10; i++)
-	{
-		st2.push(p1[i]);
-		cout << p1[i] << " ";
-	}
-	cout << endl;
-	cout << "Is st2 empty: " << st2.isempty() << endl;
-	cout << "Is st2 full: " << st2.isfull() << endl;
-
-	st1 = st2;
-	cout << "st1赋值后：" << endl;
-	cout << "Is st1 empty: " << st1.isempty() << endl;
-	cout << "Is st1 full: " << st1.isfull() << endl;
-
-	Stack st3(st2);
-	cout << "用st2构造st3后:" << endl;
-	cout << "Is st3 empty: " << st3.isempty() << endl;
-	cout << "Is st3 full: " << st3.isfull() << endl;
-
-	for (int i = 0; i < 10; i++)
-	{
-		st2.pop(p1[i]);
-		cout << p1[i] << " ";
-	}
-	cout << endl;
-	cout << "st2全pop后：" << endl;
-	cout << "Is st2 empty: " << st2.isempty() << endl;
-	cout << "Is st2 full: " << st2.isfull() << endl;
-	for (int i = 0; i < 6; i++)
-	{
-		st3.pop(p1[i]);
-		cout << p1[i] << " ";
-	}
-	cout << endl;
-	cout << "st3部分pop后：" << endl;
-	cout << "Is st3 empty: " << st3.isempty() << endl;
-	cout << "Is st3 full: " << st3.isfull() << endl;
-	system("pause");
-	return 0;
-}
+//#include <iostream>
+//#include "12.4 stack.h"
+//using namespace std;
+//
+//int main(void)
+//{
+//	Stack st1;
+//	Stack st2;
+//	Item p1[10] = { 12,20,3,0,2,2,3,5,8,9 };
+//	for (int i = 0; i < 10; i++)
+//	{
+//		st2.push(p1[i]);
+//		cout << p1[i] << " ";
+//	}
+//	cout << endl;
+//	cout << "Is st2 empty: " << st2.isempty() << endl;
+//	cout << "Is st2 full: " << st2.isfull() << endl;
+//
+//	st1 = st2;
+//	cout << "st1赋值后：" << endl;
+//	cout << "Is st1 empty: " << st1.isempty() << endl;
+//	cout << "Is st1 full: " << st1.isfull() << endl;
+//
+//	Stack st3(st2);
+//	cout << "用st2构造st3后:" << endl;
+//	cout << "Is st3 empty: " << st3.isempty() << endl;
+//	cout << "Is st3 full: " << st3.isfull() << endl;
+//
+//	for (int i = 0; i < 10; i++)
+//	{
+//		st2.pop(p1[i]);
+//		cout << p1[i] << " ";
+//	}
+//	cout << endl;
+//	cout << "st2全pop后：" << endl;
+//	cout << "Is st2 empty: " << st2.isempty() << endl;
+//	cout << "Is st2 full: " << st2.isfull() << endl;
+//	for (int i = 0; i < 6; i++)
+//	{
+//		st3.pop(p1[i]);
+//		cout << p1[i] << " ";
+//	}
+//	cout << endl;
+//	cout << "st3部分pop后：" << endl;
+//	cout << "Is st3 empty: " << st3.isempty() << endl;
+//	cout << "Is st3 full: " << st3.isfull() << endl;
+//	system("pause");
+//	return 0;
+//}
